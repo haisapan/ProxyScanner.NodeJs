b1e68f5affca0205873aae094cf45618cf4fa729
haisapan <haisapan@hotmail.com>
2015-10-26 16:48:33 +0800

    test the proxy speed
    get the proxy speed if we can connect it

--- proxyTester/proxyTester.js
+++ proxyTester/proxyTester.js
@@ -1,45 +1,26 @@
-var request=require("request");
-var Q=require("q");
-var desUrl="http://google.com.hk";
+var request = require("request");
+var Q = require("q");
+var desUrl = "http://google.com.hk";
 
 
-var testProxy=function(proxy){
-	var proxyRequest=request.defaults({
-		proxy:"http://"+proxy.Ip+":"+proxy.Port,
-		timeout:1000
+var testProxy = function (proxy) {
+	var proxyRequest = request.defaults({
+		proxy: "http://" + proxy.Ip + ":" + proxy.Port,
+		timeout: 1000,
+		time: true
 	});
-	
-	var q=Q.denodeify(proxyRequest);
-	q(desUrl).then(function(result){
-		
-		// if(err){
-		// 	return console.error(err);  //only when develop
-		// }
-		//console.log(res);
-		if(result[0].statusCode==200){
-			proxy.isValid=true;
-			console.log("Proxy Valid:  %s:%s in %s",proxy.Ip,proxy.Port, proxy.Place);
+
+	var q = Q.denodeify(proxyRequest);
+	q(desUrl).then(function (result) {
+		if (result[0].statusCode == 200) {
+			proxy.isValid = true;
+			proxy.speedTime = result[0].elapsedTime;
+			console.log("Proxy Valid:  %s:%s in %s, rate is:%s", proxy.Ip, proxy.Port, proxy.Place, proxy.speedTime);
 		}
-		
-		//return true;
-		//todo need some way to get the speed 
-	// })
-	}).catch(function(err){
+
+	}).catch(function (err) {
 		console.error(err);
 	});
-	// proxyRequest(desUrl,function(err, res){
-	// 	if(err){
-	// 		return console.error(err);  //only when develop
-	// 	}
-	// 	//console.log(res);
-	// 	if(res.statusCode==200){
-	// 		proxy.isValid=true;
-	// 		console.log("Proxy Valid:  %s:%s in %s",proxy.Ip,proxy.Port, proxy.Place);
-	// 	}
-		
-	// 	//return true;
-	// 	//todo need some way to get the speed 
-	// })
 }
 
-exports.testProxy=testProxy;
+exports.testProxy = testProxy;
