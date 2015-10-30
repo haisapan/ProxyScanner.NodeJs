16fd29b1d40a04892edd325461ffc3f2d4a7ec64
haisapan <haisapan@hotmail.com>
2015-10-30 16:48:32 +0800

    finish the xiciParser now

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-
-var cp=require("child_process");
-var tester=require("./proxyTester/proxyTester.js");
-
 var Q = require("q");
 var Scanner = require("./proxyScanner/xiciScanner.js");
+var tester=require("./proxyTester/proxyTester.js");
+
+var desUrl=process.argv[2]||"http://www.google.com.hk";  ////var desUrl ="https://github.com/";
+
+console.log("----start to connect the url: "+desUrl);
 
 var scanner = new Scanner();
 scanner.startScan().then(function (result) {
@@ -12,18 +13,8 @@ scanner.startScan().then(function (result) {
 		var ipList = scanner.parseHtml(res.body);
 
        var allTestPromises=[];
-	   ipList=[{Ip:"121.15.171.70",Port:3128},{ Ip:"124.131.221.142",Port:9999},{ Ip:"111.11.255.11",Port:80}]
 		for (var i = 0; i < ipList.length; i++) {
-// console.log(ipList[i]);
-
-			// var child=cp.fork("./proxyTester/proxyTester.js",[ipList[i]]); //,[ipList[i]]
-			// child.on("message", function(data){
-			// 	console.log("main app receive child process message:")
-			// 	console.log(JSON.stringify(data));
-			// })
-			// child.send(ipList[i]);
-
-			var proxyHandler= tester.testProxy(ipList[i]);
+			var proxyHandler= tester.testProxy(ipList[i],desUrl);
 			// allTestPromises.push(proxyHandler);
 			if (i >= 30) {
 				break;
--- proxyTester/proxyTester.js
+++ proxyTester/proxyTester.js
@@ -1,36 +1,34 @@
 var request = require("request");
 var Q = require("q");
-var desUrl ="http://www.baidu.com";//"http://google.com.hk" //"https://www.google.com.hk/#safe=strict&q=test";//"http://google.com.hk";"http://www.baidu.com"//
 
-
-var testProxy = function (proxy) {
+var testProxy = function (proxy, desUrl) {
 	var proxyRequest = request.defaults({
-		proxy:    "http://" + proxy.Ip + ":" + proxy.Port, //"http:// 60.29.248.142:8080",
-		 maxSockets:Infinity,
-		 pool: {maxSockets: Infinity},
+		proxy: "http://" + proxy.Ip + ":" + proxy.Port, //"http:// 60.29.248.142:8080",
+		maxSockets: Infinity,
+		pool: { maxSockets: Infinity },
 		timeout: 120000,
 		time: true
 	});
-	
+
 	proxyRequest({
 		url: desUrl + "?r=" + Math.random(),
 		headers: {
 			'User-Agent': 'request'
 		}
-	},function(err, result, body){
-		if(err) return; //  console.error(err);
+	}, function (err, result, body) {
+		if (err) return; //  console.error(err);
 		if (result.statusCode == 200 || result.statusCode == 502) {  //I add 502 here because sometime it work
-				proxy.isValid = true;
-				proxy.speedTime = result.elapsedTime;
-				console.log("Proxy Valid:  %s:%s in %s, rate is:%s", proxy.Ip, proxy.Port, proxy.Place, proxy.speedTime);
+			proxy.isValid = true;
+			proxy.speedTime = result.elapsedTime;
+			console.log("Proxy Valid:  %s:%s in %s, rate is:%s", proxy.Ip, proxy.Port, proxy.Place, proxy.speedTime);
 		}
-				//return [proxy];
+		//return [proxy];
 	})
-	
-return;
+
+	return;
 	var q = Q.denodeify(proxyRequest);
 	console.log(new Date());
-	 q({
+	q({
 		url: desUrl + "?r=" + Math.random(),
 		headers: {
 			'User-Agent': 'request'
@@ -56,18 +54,6 @@ return;
 	// });
 }
 
-// console.log("start child");
-// var ipItem=process.argv[2];
-// //console.log(process.argv);
-// process.on("message", function(data){
-// 	//console.log(data);
-// 	//console.log("start child message");
-// 	testProxy(data);
-// 	//console.log("Proxy Valid:  %s:%s in %s, rate is:%s", proxy.Ip, proxy.Port, proxy.Place, proxy.speedTime);
-// 				//return [proxy];
-// 				var proxy=data;
-// 	process.send({result:"Proxy start test:  %s:%s in %s, rate is:%s"+proxy.Ip+proxy.Port+proxy.Place});
-// })
 
 
 exports.testProxy = testProxy;
